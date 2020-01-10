85811e30cc20a87612d48f3371804ef77930d291
kokolem <vitek.peterk@gmail.com>
2020-01-10 17:49:10 +0100

    vypisování článků

--- get-articles.py
+++ get-articles.py
@@ -13,7 +13,15 @@ __license__ = "GPLv3"
 
 def main():
     zdroje = [line.rstrip('\n') for line in open("zdroje.txt")]
-    print(zdroje)
+    rss_feedy = [feedparser.parse(zdroj) for zdroj in zdroje]
+
+    clanky = []
+    for feed in rss_feedy:
+        clanky.append([clanek['title'] + " " + clanek['summary'] for clanek in feed['items']])
+    clanky = [item for sublist in clanky for item in sublist]
+    for clanek in clanky:
+        print(clanek)
+        print("--------------------------------------")
 
 
 if __name__ == "__main__":
