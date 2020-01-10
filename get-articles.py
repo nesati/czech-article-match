67394da4d42bf371c905cbe39e248fde0c0e9d1a
kokolem <vitek.peterk@gmail.com>
2020-01-10 17:54:00 +0100

    přidány komentáře

--- get-articles.py
+++ get-articles.py
@@ -12,13 +12,17 @@ __license__ = "GPLv3"
 
 
 def main():
-    zdroje = [line.rstrip('\n') for line in open("zdroje.txt")]
-    rss_feedy = [feedparser.parse(zdroj) for zdroj in zdroje]
+    zdroje = [line.rstrip('\n') for line in open("zdroje.txt")]  # načtení zdrojů ze zdroje.txt
+    rss_feedy = [feedparser.parse(zdroj) for zdroj in zdroje]  # stažení rss ze zdrojů
 
     clanky = []
+
+    # nadpis a tělo článku se přidají do pole clanky
     for feed in rss_feedy:
         clanky.append([clanek['title'] + " " + clanek['summary'] for clanek in feed['items']])
-    clanky = [item for sublist in clanky for item in sublist]
+
+    clanky = [item for sublist in clanky for item in sublist]  # zploštění listu
+
     for clanek in clanky:
         print(clanek)
         print("--------------------------------------")
